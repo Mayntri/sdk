063fd4778b71512b0027a57caf4fcf4eb2637ef0
Jort Kuhlmann <jortkuhlmann1@gmail.com>
2023-01-20 12:04:10 +0100

    types

--- src/merchants/index.ts
+++ src/merchants/index.ts
@@ -1,3 +1,20 @@
 import { request } from "../request";
 
-export const getAllMerchants = () => request.get('https://cat-fact.herokuapp.com/facts')
+export type getAllMerchantsResponse = {
+    status: {
+        verified: boolean
+        feedback: string
+        sentCount: number
+    }
+    _id: string
+    user: string
+    text: string
+    source: string
+    type: string
+    createdAt: string
+    updatedAt: string
+    deleted: boolean
+    used: boolean
+}
+
+export const getAllMerchants = () => request.get<getAllMerchantsResponse>('https://cat-fact.herokuapp.com/facts')
